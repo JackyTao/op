324b7a9747c3e66ef6f2aa69f06773a612f170b5
jakcy-aws <ztbjdx@163.com>
2016-07-18 16:25:07 +0000

    ADD: webtools

---
+++ webtool/inspect-server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    print request.headers
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
