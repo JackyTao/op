229157ce89179242506097903f20ed9ffc01c93a
jakcy-aws <ztbjdx@163.com>
2016-12-23 03:58:52 +0000

    .

--- webtool/inspect-server.py
+++ webtool/inspect-server.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import request
+from flask import make_response
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     print request.headers
-    return 'Hello, World!'
+
+    resp = make_response('ok', 200)
+    resp.headers['Access-Control-Allow-Origin'] =  'http://m.zhutao.xyz'
+    resp.headers['Access-Control-Allow-Credentials'] =  'true'
+    resp.set_cookie('wechat', 'wechat')
+
+    #return 'Hello, World!'
+    return resp
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8081)
